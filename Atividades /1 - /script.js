083628d13e35b809ac0a5125480eaf921fe03969
ErickSantos2000 <devErickFelip@gmail.com>
2025-10-18 15:03:54 -0300

    atividade aplicando os conceitos de desestruturação e espalhamento

---
+++ Atividades /1 - /script.js
@@ -0,0 +1,33 @@
+// Objeto inicial
+        let pessoa = {
+            nome: "Ribamar",
+            idade: 25
+        };
+
+        const nomeDOM = document.getElementById("nome");
+        const idadeDOM = document.getElementById("idade");
+        const novoNomeInput = document.getElementById("novoNome");
+        const novaIdadeInput = document.getElementById("novaIdade");
+        const registro = document.getElementById("registrar");
+
+        registro.addEventListener("click", () => {
+            // pessoa com novos valores
+            const novaPessoa = {};
+
+            if (novoNomeInput.value) {
+                novaPessoa.nome = novoNomeInput.value;
+            }
+
+            if (novaIdadeInput.value) {
+                novaPessoa.idade = (novaIdadeInput.value);
+            }
+
+            // passa as novas informações p
+            pessoa = { ...pessoa, ...novaPessoa };
+
+            // atualizando o DOM
+            const {nome, idade} = pessoa
+
+            nomeDOM.textContent = `Nome : ${nome}`;
+            idadeDOM.textContent = `Idade : ${idade}`;   
+            });
